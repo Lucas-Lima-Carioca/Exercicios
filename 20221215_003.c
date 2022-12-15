3e1cf6535be14cd6fdc52f21ab494d16f27e7203
Lucas-Lima-Carioca <120683641+Lucas-Lima-Carioca@users.noreply.github.com>
2022-12-15 17:13:25 -0400

    Create 20221215_003.c

---
+++ 20221215_003.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float metros;
+    printf("Informe o tamanho em metros: ");
+    scanf("%f", &metros);
+    printf("O valor em centimetros %c: %.2f",130,metros*100);
+
+    return 0;
+}
