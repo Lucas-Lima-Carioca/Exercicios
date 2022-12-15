ad05337c8fb0d61d5febce8fbcd6389eb30ab793
Lucas-Lima-Carioca <120683641+Lucas-Lima-Carioca@users.noreply.github.com>
2022-12-15 16:50:11 -0400

    Create 20221215_001.c

---
+++ 20221215_001.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b;
+    printf("Informe o primeiro numero: ");
+    scanf("%d", &a);
+    printf("Informe o ssegundo numero: ");
+    scanf("%d", &b);
+    printf("A soma desses numeros %c: %d",130,a + b );
+    return 0;
+}
