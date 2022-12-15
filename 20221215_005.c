1548552fdd9e5df2dc41694ea1d859cc94f1732a
Lucas-Lima-Carioca <120683641+Lucas-Lima-Carioca@users.noreply.github.com>
2022-12-15 17:18:39 -0400

    Create 20221215_005.c

---
+++ 20221215_005.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float lado;
+    printf("Informe o lado do quadrado: ");
+    scanf("%f", &lado);
+    printf("A area do quadrado %c: %.2f\n",130,lado*lado);
+    printf("O dobro da area do quadrado %c: %.2f",130,lado*lado*2);
+
+
+    return 0;
+}
