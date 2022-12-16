3de310a145308ac7ed380d8d6521a0fa6324db09
Lairu-Fire <60993665+Lairu-Fire@users.noreply.github.com>
2022-12-16 19:46:09 -0400

    Create 20221215_010.c

---
+++ 20221215_010.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float altura;
+    printf("Informe sua altura: ");
+    scanf("%f", &altura);
+    printf("Sua massa ideal %c: %.2f",130, 72.7*altura-58);
+    return 0;
+}
